381a563936c66c7345257c05af42e9ac2118a482
Konstantin Lazukin <klazukin@amcbridge.com>
2016-09-25 13:20:36 +0300

    Some glew stuff

--- ThirdParty/Prepare.cmake
+++ ThirdParty/Prepare.cmake
@@ -3,10 +3,9 @@ set(GLFW_DIR ${TOP}/ThirdParty/glfw/)
 set(GLFW_SOURCE_DIR ${GLFW_DIR}Sources/)
 set(GLFW_BUILD_DIR ${GLFW_DIR}Build/${BUILD_ARCHITECTURE}/)
 
-if(NOT EXISTS ${GLFW_SOURCE_DIR})
-    #Download glwf sources
-    execute_process(COMMAND git clone https://github.com/glfw/glfw.git ${GLFW_SOURCE_DIR})
-endif()
+set(GLEW_DIR ${TOP}/ThirdParty/glew/)
+set(GLEW_SOURCE_DIR ${GLEW_DIR}Sources/)
+set(GLEW_BUILD_DIR ${GLEW_DIR}Build/${BUILD_ARCHITECTURE}/)
 
 function(BuildGlfw Configuration)
     if(NOT EXISTS ${GLFW_BUILD_DIR}/${Configuration}/${BUILD_ARCHITECTURE})
@@ -22,15 +21,22 @@ function(BuildGlfw Configuration)
     endif()
 endfunction()
 
-if(NOT EXISTS ${GLFW_DIR}/Debug/${Configuration}/${BUILD_ARCHITECTURE})    
-    if(${BUILD_ARCHITECTURE} STREQUAL X32)
-        set(GENERATOR_NAME "Visual Studio 14 2015")
-    else()
-        set(GENERATOR_NAME "Visual Studio 14 2015 Win64")
-    endif()
-    
+if(${BUILD_ARCHITECTURE} STREQUAL X32)
+    set(GENERATOR_NAME "Visual Studio 14 2015")
+else()
+    set(GENERATOR_NAME "Visual Studio 14 2015 Win64")
+endif()
+
+#glfw
+if(NOT EXISTS ${GLFW_DIR}/Debug/${Configuration}/${BUILD_ARCHITECTURE})
     #Generate build files
     if(NOT EXISTS ${GLFW_BUILD_DIR})
+
+        #Download glwf sources
+        if(NOT EXISTS ${GLFW_SOURCE_DIR})
+            execute_process(COMMAND git clone https://github.com/glfw/glfw.git ${GLFW_SOURCE_DIR})
+        endif()
+
         file(MAKE_DIRECTORY ${GLFW_BUILD_DIR})
         execute_process(COMMAND cmake -G${GENERATOR_NAME} ${GLFW_SOURCE_DIR} WORKING_DIRECTORY ${GLFW_BUILD_DIR})
     endif()
@@ -46,3 +52,7 @@ if(NOT EXISTS ${GLFW_DIR}/Debug/${Configuration}/${BUILD_ARCHITECTURE})
     file(REMOVE_RECURSE ${GLFW_BUILD_DIR})
 endif()
 
+#glew
+if(NOT EXISTS ${GLEW_DIR}/Debug/${Configuration}/${BUILD_ARCHITECTURE})
+
+endif()
