ae998dac0b739d0ea5639f5cf0aa839f1138039b
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2016-10-06 15:05:10 +0300

    Create launcher via cmake (visual studio only). Remove trash message.

---
+++ Build/Modules/CreateLauncher.cmake
@@ -0,0 +1,35 @@
+function(CreateLauncher targetName)
+	set(TemplatesDir ${TOP}/Build/Modules/LaunchersTemplates)
+
+	if(${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
+	
+		file(READ "${TemplatesDir}/Perconfig.vcxproj.user.in" PerConfig)
+		
+		set(USERFILE_CONFIGSECTIONS)
+    
+		foreach(USERFILE_CONFIGNAME ${CMAKE_CONFIGURATION_TYPES})
+			set(USERFILE_${USERFILE_CONFIGNAME}_COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${USERFILE_CONFIGNAME}/${targetName})
+			set(USERFILE_WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${USERFILE_CONFIGNAME})
+						
+			if(${BUILD_ARCHITECTURE} STREQUAL "X32")
+				set(USERFILE_PLATFORM Win32)
+			else()
+				set(USERFILE_PLATFORM ${BUILD_ARCHITECTURE})
+			endif()
+		
+			string(CONFIGURE "${PerConfig}" _temp @ONLY ESCAPE_QUOTES)
+			string(CONFIGURE
+				"${USERFILE_CONFIGSECTIONS}${_temp}"
+				USERFILE_CONFIGSECTIONS
+				ESCAPE_QUOTES)
+		endforeach()
+	
+		configure_file("${TemplatesDir}/vcxproj.user.in"
+						"ALL_BUILD.vcxproj.user"
+						@ONLY
+		)
+		
+	else()
+		message(WARNING "Can't create launcher for ${COMPILER_NAME} compiler type.")
+	endif()	
+endfunction()
