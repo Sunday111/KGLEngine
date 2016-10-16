71e32b3155ed6e56bec701cfce795724152d7bc6
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2016-10-16 23:28:31 +0300

    Core object is base type for graphics object

--- Code/KGL_Engine/KGL_Graphics/include/KGL_Graphics/IObject.h
+++ Code/KGL_Engine/KGL_Graphics/include/KGL_Graphics/IObject.h
@@ -2,10 +2,11 @@
 #define KGL_GRAPHICS_IOBJECT_H_INCLUDED
 
 #include <KGL_Graphics/Common.h>
+#include <KGL_Core/IObject.h>
 
 namespace KGL { namespace Graphics {
 
-class KGL_GRAPHICS_API IObject
+class KGL_GRAPHICS_API IObject : public Core::IObject
 {
 public:
 	virtual ~IObject() = 0;
